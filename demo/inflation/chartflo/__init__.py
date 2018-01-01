ec8556fbd895c1c64bf1f671acb7450f96f901d3
Win <synwe@yahoo.com>
2018-01-01 11:05:16 +0000

    Add generator function for years detail view layout

--- demo/inflation/chartflo/__init__.py
+++ demo/inflation/chartflo/__init__.py
@@ -2,6 +2,7 @@
 from chartflo.apps import cf
 from inflation.models import Measurement
 from .charts import charts
+from .layouts import layout_index
 
 
 def run(**kwargs):
@@ -11,5 +12,8 @@ def run(**kwargs):
     cf.report_path = "templates/dashboards/inflation"
     cf.rename("date", "Date")
     cf.rename("value", "Value")
+    cf.rename("month", "Month")
     cf.backup()
     charts.make(cf)
+    cf.restore()
+    layout_index(cf)
